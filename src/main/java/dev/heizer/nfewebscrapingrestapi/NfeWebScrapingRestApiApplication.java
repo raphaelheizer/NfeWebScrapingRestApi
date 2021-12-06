4f1553e9cf973bb2d418394a1aade9ebd0524583
= <raphaelheizer@gmail.com>
2021-12-06 00:08:50 -0300

    Initial commit

---
+++ src/main/java/dev/heizer/nfewebscrapingrestapi/NfeWebScrapingRestApiApplication.java
@@ -0,0 +1,10 @@
+package dev.heizer.nfewebscrapingrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfeWebScrapingRestApiApplication
+{
+    public static void main(String[] args) {SpringApplication.run(NfeWebScrapingRestApiApplication.class, args);}
+}
---
+++ src/test/java/dev/heizer/nfewebscrapingrestapi/NfeWebScrapingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.heizer.nfewebscrapingrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfeWebScrapingRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
